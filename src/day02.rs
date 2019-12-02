20a2b33eb34b364945062f8bb7f2efd50cfd9180
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2019-12-02 10:22:04 +0100

    slightly refactor intcode computer; complete part 2

--- src/day02.rs
+++ src/day02.rs
@@ -3,53 +3,75 @@ use std::path::Path;
 
 pub struct Day02;
 
-type Word = u32;
+pub type Word = u32;
+pub type IntcodeMemory = Vec<Word>;
 
 // return true if we should continue, or false if not
-fn process(position: usize, program: &mut [Word]) -> bool {
-    match &program[position..position + 4] {
+fn process(ip: usize, memory: &mut [Word]) -> bool {
+    match &memory[ip..ip + 4] {
         [opcode, a, b, out] => {
             // TODO
             match *opcode {
                 1 => {
-                    program[*out as usize] = program[*a as usize] + program[*b as usize];
+                    memory[*out as usize] = memory[*a as usize] + memory[*b as usize];
                     true
                 }
                 2 => {
-                    program[*out as usize] = program[*a as usize] * program[*b as usize];
+                    memory[*out as usize] = memory[*a as usize] * memory[*b as usize];
                     true
                 }
                 99 => false,
                 _ => {
-                    eprintln!("invalid opcode @ {}: {}", position, opcode);
+                    eprintln!("invalid opcode @ {}: {}", ip, opcode);
                     false
                 }
             }
         }
         [opcode] if *opcode == 99 => false,
         other => {
-            eprintln!("malformed program opcodes @ {}: {:?}", position, other);
+            eprintln!("malformed memory opcodes @ {}: {:?}", ip, other);
             false
         }
     }
 }
 
+pub fn compute_intcode(memory: &mut IntcodeMemory) {
+    let mut ip = 0;
+    while process(ip, memory) {
+        ip += 4;
+    }
+}
+
 impl Exercise for Day02 {
     fn part1(&self, path: &Path) {
-        let mut program: Vec<Word> = parse::<CommaSep<Word>>(path).unwrap().flatten().collect();
-        let mut position = 0;
-        
-        // comment out these initialization lines for test input
-        program[1] = 12;
-        program[2] = 2;
+        let mut memory: IntcodeMemory = parse::<CommaSep<Word>>(path).unwrap().flatten().collect();
 
-        while process(position, &mut program) {
-            position += 4;
-        }
-        println!("value at position 0: {}", program[0]);
+        // comment out these initialization lines for test input
+        memory[1] = 12;
+        memory[2] = 2;
+
+        compute_intcode(&mut memory);
+        println!("value at ip 0: {}", memory[0]);
     }
 
-    fn part2(&self, _: &Path) {
-        unimplemented!()
+    fn part2(&self, path: &Path) {
+        let initial_memory: IntcodeMemory =
+            parse::<CommaSep<Word>>(path).unwrap().flatten().collect();
+
+        const TARGET: Word = 19690720;
+
+        'stop: for noun in 0..=99 {
+            for verb in 0..=99 {
+                let mut memory = initial_memory.clone();
+                memory[1] = noun;
+                memory[2] = verb;
+                compute_intcode(&mut memory);
+                if memory[0] == TARGET {
+                    println!("100 * noun + verb: {}", (100 * noun) + verb);
+                    break 'stop;
+                }
+            }
+        }
+        println!("done part 2");
     }
 }
