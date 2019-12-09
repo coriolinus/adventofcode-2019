3bb5aeb0fe8f89ab37d165cbea273851c590ff00
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2019-12-09 10:11:21 +0100

    day 9 part 1

--- src/day09.rs
+++ src/day09.rs
@@ -9,6 +9,21 @@ pub struct Day;
 impl Exercise for Day {
     fn part1(&self, path: &Path) {
         let memory: IntcodeMemory = parse::<CommaSep<Word>>(path).unwrap().flatten().collect();
+        let mut computer = Intcode::new(memory).using_inputs(&[1]);
+        match computer.run_collect() {
+            Ok(outputs) => match outputs.len() {
+                0 => println!("no output"),
+                1 => println!("BOOST keycode: {}", outputs[0]),
+                _ => {
+                    println!(
+                        "self-diagnostic problems: {:?}",
+                        &outputs[..outputs.len() - 1]
+                    );
+                    println!("BOOST keycode??: {}", outputs[outputs.len() - 1]);
+                }
+            },
+            Err(err) => println!("intcode error: {}", err),
+        }
     }
 
     fn part2(&self, _path: &Path) {
@@ -32,7 +47,7 @@ mod tests {
 
     #[test]
     fn test_16_digits() {
-        let mut computer = Intcode::new(vec![1102,34915192,34915192,7,4,7,99,0]);
+        let mut computer = Intcode::new(vec![1102, 34915192, 34915192, 7, 4, 7, 99, 0]);
         let output = computer.run_collect().unwrap();
         assert!(!output.is_empty());
         assert_eq!(output[0].to_string().len(), 16);
@@ -40,7 +55,7 @@ mod tests {
 
     #[test]
     fn test_big_number() {
-        let program = vec![104,1125899906842624,99];
+        let program = vec![104, 1125899906842624, 99];
         let expect = program[1];
         let mut computer = Intcode::new(program);
         let output = computer.run_collect().unwrap();
