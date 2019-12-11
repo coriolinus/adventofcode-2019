4f951bfdce7951600f1264cef691d147f1c32428
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2019-12-11 13:05:01 +0100

    enable passing args through the bencher

--- bench.sh
+++ bench.sh
@@ -5,6 +5,7 @@ set -e
 cargo fmt
 
 input="$1"
+shift 1
 
 if [ -z "$input" ]; then
     echo "usage: $0 INPUT"
@@ -13,4 +14,4 @@ fi
 
 set -x
 
-hyperfine "target/release/aoc2019 $input" "target/release/aoc2019 $input --no-part1 --part2"
+hyperfine "target/release/aoc2019 $input $*" "target/release/aoc2019 $input --no-part1 --part2 $*"
