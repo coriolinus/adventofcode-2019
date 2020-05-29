08cb2f1a091e96474355d420c513f78eb7565b49
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2020-05-29 20:20:57 +0200

    complete day14 part2
    That took a bit of thought before I decided that a binary search was
    the right way to go.
    
    I'm sure it's slower than a more optimal solution, but it's still
    not too bad:
    
    $ ./bench.sh inputs/14.txt
    Fri 29 May 2020 08:23:51 PM CEST
    + hyperfine 'target/release/aoc2019 inputs/14.txt ' 'target/release/aoc2019 inputs/14.txt --no-part1 --part2 '
    Benchmark #1: target/release/aoc2019 inputs/14.txt
      Time (mean ± σ):       3.7 ms ±   1.3 ms    [User: 2.2 ms, System: 1.4 ms]
      Range (min … max):     0.7 ms …   5.3 ms    1432 runs
    
      Warning: Command took less than 5 ms to complete. Results might be inaccurate.
      Warning: Statistical outliers were detected. Consider re-running this benchmark on a quiet PC without any interferences from other programs. It might help to use the '--warmup' or '--prepare' options.
    
    Benchmark #2: target/release/aoc2019 inputs/14.txt --no-part1 --part2
      Time (mean ± σ):       4.0 ms ±   1.1 ms    [User: 2.4 ms, System: 1.5 ms]
      Range (min … max):     0.7 ms …   5.1 ms    634 runs
    
      Warning: Command took less than 5 ms to complete. Results might be inaccurate.
    
    Summary
      'target/release/aoc2019 inputs/14.txt ' ran
        1.07 ± 0.47 times faster than 'target/release/aoc2019 inputs/14.txt --no-part1 --part2 '

--- src/day14.rs
+++ src/day14.rs
@@ -12,49 +12,83 @@ pub struct Day;
 
 impl Exercise for Day {
     fn part1(&self, path: &Path) {
-        let reactions = parse::<Reaction>(path).unwrap().collect::<Vec<_>>();
-        let producers: HashMap<_, _> = reactions.iter().map(|reaction| (reaction.outputs.elem.clone(), reaction.clone())).collect();
-        assert!(producers.contains_key(FUEL), "fuel must be an output");
-
-        let mut want = VecDeque::new();
-        want.push_back(Reagent{qty: 1, elem: FUEL.into()});
-        let mut reactions_used: HashMap<String, ReactionQty> = HashMap::new();
-
-
-        while let Some(reagent) = want.pop_front() {
-            let reaction = producers.get(&reagent.elem).expect("reaction wasn't found!");
-            let increased_reactions = reactions_used.entry(reagent.elem).or_default().add_wanted_outputs(reagent.qty, reaction.outputs.qty);
-            if increased_reactions > 0 {
-                for mut input in reaction.inputs.iter().cloned() {
-                    if input.elem != ORE {
-                        input.qty *= increased_reactions;
-                        want.push_back(input);
-                    }
-                }
-            }
-        }
-
-        let mut ore_used = 0;
-        for (product, qty) in reactions_used {
-            let reaction = producers.get(&product).unwrap();
-            for input in reaction.inputs.iter() {
-                if input.elem == ORE {
-                    ore_used += qty.used * input.qty;
-                }
-            }
-        }
+        let producers = make_producers(path);
+        let ore_used = ore_for(1, &producers);
 
         println!("ore per 1 fuel: {}", ore_used);
     }
 
-    fn part2(&self, _path: &Path) {
-        unimplemented!()
+    fn part2(&self, path: &Path) {
+        const ORE_MINED: u64 = 1000000000000;
+        let producers = make_producers(path);
+
+        // binary search guess and check to find the greatest amount of fuel
+        // refinable with this much ore
+        let mut low = ORE_MINED / ore_for(1, &producers);
+        let mut high = low*2;
+        let mut prev_guess = 0;
+        let mut guess = (low + high) / 2;
+
+        while guess != prev_guess {
+            #[cfg(feature = "debug")]
+            dbg!(low, guess, high);
+
+            let needed = ore_for(guess, &producers);
+            match needed.cmp(&ORE_MINED) {
+                std::cmp::Ordering::Equal => break,
+                std::cmp::Ordering::Less => low = guess,
+                std::cmp::Ordering::Greater => high = guess,
+            }
+            prev_guess = guess;
+            guess = (low + high) / 2;
+        }
+
+        println!("max fuel for 1 trillion ore: {}", guess);
     }
+}
+
+fn make_producers(path: &Path) -> HashMap<String, Reaction> {
+    let reactions = parse::<Reaction>(path).unwrap().collect::<Vec<_>>();
+    let producers: HashMap<_, _> = reactions.into_iter().map(|reaction| (reaction.outputs.elem.clone(), reaction)).collect();
+    assert!(producers.contains_key(FUEL), "fuel must be an output");
+    producers
+}
+
+fn ore_for(fuel: u64, producers: &HashMap<String, Reaction>) -> u64 {
+    let mut want = VecDeque::new();
+    want.push_back(Reagent{qty: fuel, elem: FUEL.into()});
+    let mut reactions_used: HashMap<String, ReactionQty> = HashMap::new();
+
+
+    while let Some(reagent) = want.pop_front() {
+        let reaction = producers.get(&reagent.elem).expect("reaction wasn't found!");
+        let increased_reactions = reactions_used.entry(reagent.elem).or_default().add_wanted_outputs(reagent.qty, reaction.outputs.qty);
+        if increased_reactions > 0 {
+            for mut input in reaction.inputs.iter().cloned() {
+                if input.elem != ORE {
+                    input.qty *= increased_reactions;
+                    want.push_back(input);
+                }
+            }
+        }
+    }
+
+    let mut ore_used = 0;
+    for (product, qty) in reactions_used {
+        let reaction = producers.get(&product).unwrap();
+        for input in reaction.inputs.iter() {
+            if input.elem == ORE {
+                ore_used += qty.used * input.qty;
+            }
+        }
+    }
+
+    ore_used
 }
 
 #[derive(PartialEq, Eq, Hash, Clone)]
 struct Reagent {
-    qty: u32,
+    qty: u64,
     elem: String,
 }
 
@@ -66,7 +100,7 @@ impl FromStr for Reagent {
             Err(format!("reagent expects 2 tokens; got {}", tokens.len()))?;
         }
         Ok(Reagent {
-            qty: tokens[0].parse::<u32>().map_err(|e| e.to_string())?,
+            qty: tokens[0].parse::<u64>().map_err(|e| e.to_string())?,
             elem: tokens[1].into(),
         })
     }
@@ -98,12 +132,12 @@ impl FromStr for Reaction {
 
 #[derive(Default, Debug)]
 struct ReactionQty {
-    used: u32,
-    total_outputs_wanted: u32,
+    used: u64,
+    total_outputs_wanted: u64,
 }
 
 impl ReactionQty {
-    fn add_wanted_outputs(&mut self, additional_outputs: u32, outputs_per_reaction: u32) -> u32 {
+    fn add_wanted_outputs(&mut self, additional_outputs: u64, outputs_per_reaction: u64) -> u64 {
         let old_used = self.used;
         self.total_outputs_wanted += additional_outputs;
         if self.used * outputs_per_reaction < self.total_outputs_wanted{
